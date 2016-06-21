312a9187ceb13206795b16d05a0e2c6272f78803
Ruslan <yarullin@softmajor.ru>
2016-06-21 21:19:53 +0500

    modifies for alpha-release

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Sample for using Esockets in file sample/index.php
 
 ### Which PHP version is supported?
 Supported PHP5.4 or higher version
+
+## Samples of use: [read the wiki](https://github.com/maestroprog/esockets-php/wiki) [https://github.com/maestroprog/esockets-php/wiki](https://github.com/maestroprog/esockets-php/wiki)
--- tests/TestEsocketsLinear.php
+++ tests/TestEsocketsLinear.php
@@ -11,35 +11,51 @@ namespace Esockets;
 
 class TestEsockets extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var $server \Esockets\Server
-     * @var $client \Esockets\Client
-     */
-    private $server, $client;
+    /** @var Server */
+    static private $server;
+    /** @var Client */
+    static private $client;
+    /** @var int */
+    static private $peer_accepted = 0;
 
     public function testServerOpen()
     {
-        $this->server = new \Esockets\Server();
-        $this->assertTrue($this->server->connect(), 'Сервер не создаётся');
+        self::$server = new Server();
+        $this->assertTrue(self::$server->connect(), 'Сервер не создаётся');
     }
 
     public function testClientConnect()
     {
-        $this->client = new \Esockets\Client();
-        $this->assertTrue($this->client->connect(), 'Клиент не может соединиться');
+        self::$client = new Client();
+        $this->assertTrue(self::$client->connect(), 'Клиент не может соединиться');
     }
 
     public function testServerAcceptClient()
     {
-        $this->server->onConnectPeer(function (\Esockets\Peer $peer) {
-
+        self::$server->onConnectPeer(function (Peer $peer) {
+            self::$peer_accepted++;
         });
+        self::$server->listen();
+    }
 
-        $this->server->listen();
+    public function testServerAcceptPeer()
+    {
+        $this->assertTrue(self::$peer_accepted > 0);
     }
 
     public function testClientSendData()
     {
-        $this->assertTrue($this->client->send('Hello world'), 'Клиент не может отправить данные');
+        $this->assertTrue(self::$client->send('Hello world'), 'Клиент не может отправить данные');
+    }
+
+    public function testServerReceiveData()
+    {
+        //self::$server->
+    }
+
+    public function testClientDisconnect()
+    {
+        self::$client->disconnect();
+        self::assertTrue(self::$client->is_connected());
     }
 }
